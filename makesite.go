542cc6752637e6dd3823235a40c46a962134b210
Dino-Martinez <>
2022-02-09 13:58:34 -0500

    Finished first portion of MVP challenges

--- makesite.go
+++ makesite.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"html/template"
+	"os"
+)
+
+// Page holds all the information we need to generate a new
+// HTML page from a text file on the filesystem.
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content      string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	filePath := "first-post.txt"
+
+	fileContents, err := ioutil.ReadFile(filePath)
+
+
+	if err != nil {
+		// A common use of `panic` is to abort if a function returns an error
+		// value that we don’t know how to (or want to) handle. This example
+		// panics if we get an unexpected error when creating a new file.
+		panic(err)
+	}
+
+	fmt.Println(string(fileContents))
+
+	page := Page{
+		TextFilePath: filePath,
+		TextFileName: "first-post",
+		HTMLPagePath: "first-post.html",
+		Content:      string(fileContents),
+	}
+
+	// Create a new template in memory named "template.tmpl".
+	// When the template is executed, it will parse template.tmpl,
+	// looking for {{ }} where we can inject content.
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// Create a new, blank HTML file.
+	newFile, err := os.Create(page.HTMLPagePath)
+	if err != nil {
+				panic(err)
+	}
+
+	// Executing the template injects the Page instance's data,
+	// allowing us to render the content of our text file.
+	// Furthermore, upon execution, the rendered template will be
+	// saved inside the new file we created earlier.
+	t.Execute(newFile, page)
+
 }
